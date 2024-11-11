9764f15951cdc2797d11d6fe0d4c09705c45ffee
Andrii Nakryiko <andrii@kernel.org>
2024-11-11 15:30:37 -0800

    tests: add USDT inlining test
    Validate that semaphores are preserved and shared, and we get multiple
    USDT definitions.
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- tests/common.h
+++ tests/common.h
@@ -4,6 +4,9 @@
 
 #define __weak __attribute__((weak))
 #define __optimize __attribute__((optimize(2)))
+#ifndef __always_inline
+#define __always_inline inline __attribute__((always_inline))
+#endif
 
 #ifdef __cplusplus
 extern "C" {
