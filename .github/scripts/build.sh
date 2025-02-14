264d14c551e7ad25f7dd2a794e788577a9feeee1
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-02-13 16:57:40 -0800

    ci: LLVM support
    Update CI scripts to also build the tests with various versions of
    LLVM. Versions below 17 can't be installed with the official llvm.sh
    script, so for now let's test only 17+.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,12 +2,16 @@
 
 set -euo pipefail
 
-GCC_VERSION=${GCC_VERSION:-13}
-SHARED=${SHARED:-0}
-
-export CC=gcc-${GCC_VERSION}
-export CXX=g++-${GCC_VERSION}
-export AR=gcc-ar-${GCC_VERSION}
+if [ "$COMPILER" == "llvm" ]; then
+        export CC="clang-${COMPILER_VERSION}"
+        export CXX="clang++-${COMPILER_VERSION}"
+        export AR="llvm-ar-${COMPILER_VERSION}"
+        export LD="lld-${COMPILER_VERSION}"
+elif [ "$COMPILER" == "gcc" ]; then
+        export CC="gcc-${COMPILER_VERSION}"
+        export CXX="g++-${COMPILER_VERSION}"
+        export AR="gcc-ar-${COMPILER_VERSION}"
+fi
 
 make -C tests clean
-make SHARED=${SHARED} -C tests -j$(nproc) build
+make SHARED=${SHARED:-0} -C tests -j$(nproc) build
