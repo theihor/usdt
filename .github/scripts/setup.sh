d1f46773c73d4c29a5dd4f0d17939780314795ca
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-02-13 16:57:40 -0800

    ci: LLVM support
    Update CI scripts to also build the tests with various versions of
    LLVM. Versions below 17 can't be installed with the official llvm.sh
    script, so for now let's test only 17+.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -5,15 +5,22 @@ set -euo pipefail
 # Assume sudo in this script
 GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
 BPFTRACE_VERSION=${BPFTRACE_VERSION:-0.22.1}
-GCC_VERSION=${GCC_VERSION:-13}
+COMPILER=${COMPILER:-gcc}
+COMPILER_VERSION=${COMPILER_VERSION:-13}
 
 # Install pre-requisites
-export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
-apt-get install -y curl file gawk libfuse2t64 make sudo
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
+apt-get install -y curl file gawk gnupg libfuse2t64 lsb-release make software-properties-common sudo wget
 
 # Install CC
-apt-get install -y gcc-${GCC_VERSION} g++-${GCC_VERSION}
+if [ "$COMPILER" == "llvm" ]; then
+        curl -O https://apt.llvm.org/llvm.sh
+        chmod +x llvm.sh
+        ./llvm.sh ${COMPILER_VERSION}
+else
+        apt-get install -y gcc-${COMPILER_VERSION} g++-${COMPILER_VERSION}
+fi
 
 ${GITHUB_WORKSPACE}/.github/scripts/install-bpftrace.sh
 
